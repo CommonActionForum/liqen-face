afcaa68e2912ca25dee455e194e951a95cca768a
Carlos Saito <c.saito.style@gmail.com>
2017-09-05 00:53:28 +0200

    Mock of the feature

--- server/router.js
+++ server/router.js
@@ -29,6 +29,36 @@ router.get('/login', (req, res) => {
   res.render('index')
 })
 
+router.get('/--dev/login/medium', (req, res) => {
+  // Request a state to the core
+  const state = 'blablabla'
+
+  const redirectUri =
+    encodeURIComponent(`${process.env.MEDIUM_REDIRECT_URI}/--dev/callback/medium`)
+
+  // Redirect the user
+  const uri = `https://medium.com/m/oauth/authorize?`
+    + `client_id=${process.env.MEDIUM_CLIENT_ID}`
+    + `&scope=basicProfile`
+    + `&state=${state}`
+    + `&response_type=code`
+    + `&redirect_uri=${redirectUri}`
+
+  res.redirect(uri)
+})
+
+router.get('/--dev/callback/medium', (req, res) => {
+  // Get state and code
+  // Request to the core
+  // Get the token
+  // Redirect to /import-content/medium
+  res.redirect('/--dev/import-content/medium')
+})
+
+router.get('/--dev/import-content/medium', (req, res) => {
+  res.render('import-medium')
+})
+
 router.get('*', (req, res) => {
   res.send('404 Not found')
 })
